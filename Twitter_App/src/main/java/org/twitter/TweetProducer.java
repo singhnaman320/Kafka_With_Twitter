46ed17bf9ede704f8ba5c4e0b503c9e8ee5977ef
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-14 11:02:18 +0530

    Twitter with Kafka

---
+++ Twitter_App/src/main/java/org/twitter/TweetProducer.java
@@ -0,0 +1,24 @@
+package org.twitter;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.event.Event;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/*
+    For this use: https://github.com/twitter/hbc
+    */
+public class TweetProducer {
+    public static void main(String[] args) {
+
+
+    }
+}
