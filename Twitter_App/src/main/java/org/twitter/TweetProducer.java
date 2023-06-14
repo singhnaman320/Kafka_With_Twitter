415c52e9f2d42fff2645df3b77526773517a7cf0
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-14 11:53:59 +0530

    Creating Twitter Producer

--- Twitter_App/src/main/java/org/twitter/TweetProducer.java
+++ Twitter_App/src/main/java/org/twitter/TweetProducer.java
@@ -49,6 +49,9 @@ public class TweetProducer {
 
         Properties properties = new Properties();
 
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
     }
 
